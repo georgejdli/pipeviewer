ec141c5fcd43c38350d92a78906b3038fcf730aa
George Li <georgejdli@gmail.com>
2020-09-27 15:33:45 -0500

    Completed 2.4:

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use ::std::env;
-use std::io::{self, Read, Write};
+use std::io::{self, ErrorKind, Read, Result, Write};
 
 // 16 byte size buffer
 const CHUNK_SIZE: usize = 16 * 1024;
 
-fn main() {
+fn main() -> Result<()> {
     let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
     let mut total_bytes = 0;
+    let mut buffer = [0; CHUNK_SIZE];
     loop {
-        let mut buffer = [0; CHUNK_SIZE];
         // debug macro better than straight println and having to format the string
         // not for logging!!! don't leave these in actual production code
         //dbg!(silent);
@@ -18,9 +18,23 @@ fn main() {
             Err(_) => break,
         };
         total_bytes += num_read;
-        io::stdout().write_all(&buffer[..num_read]).unwrap();
+        if !silent {
+            // return to start of the line to it overwrites prevous output, to show progress cleanly
+            // not using new "ln"
+            eprint!("\r{}", total_bytes);
+        }
+
+        if let Err(e) = io::stdout().write_all(&buffer[..num_read]) {
+            if e.kind() == ErrorKind::BrokenPipe {
+                break;
+            }
+            return Err(e);
+            // eprintln!("Oh no, an error! {}", e.to_string());
+            // std::process::exit(1)
+        }
     }
     if !silent {
-        eprintln!("num_read: {}", total_bytes);
+        eprintln!("\r{}", total_bytes);
     }
+    Ok(())
 }
