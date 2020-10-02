9e6ac4c7a59284133ff266fb8af66a2361ccff62
George Li <georgejdli@gmail.com>
2020-10-01 22:43:41 -0500

    finished 3.4 - using mpsc channels

--- src/read.rs
+++ src/read.rs
@@ -1,9 +1,9 @@
 use crate::CHUNK_SIZE;
 use std::fs::File;
 use std::io::{self, BufReader, Read, Result};
-use std::sync::{Arc, Mutex};
+use std::sync::mpsc::Sender;
 
-pub fn read_loop(infile: &str, quit: Arc<Mutex<bool>>) -> Result<()> {
+pub fn read_loop(infile: &str, stats_tx: Sender<Vec<u8>>) -> Result<()> {
     let mut reader: Box<dyn Read> = if !infile.is_empty() {
         Box::new(BufReader::new(File::open(infile)?))
     } else {
@@ -18,12 +18,12 @@ pub fn read_loop(infile: &str, quit: Arc<Mutex<bool>>) -> Result<()> {
             Ok(x) => x,
             Err(_) => break,
         };
-        // todo: send buffer to stats thread
-        Vec::from(&buffer[..num_read]);
-    }
 
-    // todo: send an empty buffer to stats thread
-    let mut quit = quit.lock().unwrap();
-    *quit = true;
+        if stats_tx.send(Vec::from(&buffer[..num_read])).is_err() {
+            break;
+        }
+    }
+    // ignore result since we are shutting down anyway
+    let _ = stats_tx.send(Vec::new());
     Ok(())
 }
